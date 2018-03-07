07acf620cd87313ce7c44b8ef243ca6476f9cb1e
Barney <barneym29@gmail.com>
2018-03-07 15:11:22 +0300

    Setup

---
+++ view-animations-starter/NextLevelViewController.swift
@@ -0,0 +1,13 @@
+
+import UIKit
+
+class NextLevelViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
