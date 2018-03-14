becd6187ed0ee9b4de8deda242c4881ffbbe8adc
Barney <barneym29@gmail.com>
2018-03-14 10:48:42 +0300

    View transitions

--- view-animations-starter/DarkViewController.swift
+++ view-animations-starter/DarkViewController.swift
@@ -1,11 +1,3 @@
-//
-//  DarkViewController.swift
-//  view-animations-starter
-//
-//  Created by Barney on 07/03/2018.
-//  Copyright © 2018 Barney. All rights reserved.
-//
-
 import UIKit
 
 class DarkViewController: UIViewController {
@@ -15,21 +7,5 @@ class DarkViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- view-animations-starter/NextLevelViewController.swift
+++ view-animations-starter/NextLevelViewController.swift
@@ -3,11 +3,42 @@ import UIKit
 
 class NextLevelViewController: UIViewController {
 
+    @IBOutlet weak var goldenView: UIView!
+    @IBOutlet weak var greenView: UIView!
+
+    private var isFlipped: Bool = false
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func transition(_ sender: UIButton) {
+        isFlipped = !isFlipped
+
+        let cardToFlip = isFlipped ? goldenView : greenView
+        let bottomCard = isFlipped ? greenView : goldenView
+
+        UIView.transition(from: cardToFlip!,
+                          to: bottomCard!,
+                          duration: 0.5,
+                          options: [.transitionCurlUp, .showHideTransitionViews],
+                          completion: nil)
+
+
+//        UIView.transition(with: cardToFlip!,
+//                          duration: 0.5,
+//                          options: [.transitionFlipFromRight],
+//                          animations: {
+//
+//                            cardToFlip?.isHidden =  true
+//        },
+//                          completion: { _ in
+//
+//                            self.view.bringSubview(toFront: bottomCard!)
+//                            cardToFlip?.isHidden = false
+//        })
+    }
 
 }
